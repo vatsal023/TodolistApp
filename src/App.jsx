335e3bad66d1fc45cefa3de62a6a0557aad518a3
vatsal023 <vatsalgarg504@gmail.com>
2025-08-11 00:48:09 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -92,14 +92,15 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
-        <div className="addTodo my-5">
-          <h2 className="text-lg font-bold">Add a todo</h2>
-          <input onChange={handleChange} value={todo} type="text" className="w-1/2" />
-          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6 ">Save</button>
+      <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
+        <h1 className = "font-bold text-center text-xl">iTask-Manage your todos at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className="text-lg font-bold">Add a todo</h2> 
+          <input onChange={handleChange} value={todo} type="text" className="w-full rounded-full px-5 py-1" /> 
+          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md ">Save</button>
         </div>
 
-        <input onChange = {toggleFinished} type="checkbox" checked = {showFinished}/>Show Finished
+        <input className = "my-4" onChange = {toggleFinished} type="checkbox" checked = {showFinished}/>Show Finished
         <h2 className='text-xl font-bold'>Your Todos</h2>
 
         <div className="todos"> 
@@ -108,7 +109,7 @@ function App() {
 
           {todos.map(item => {
 
-            return (showFinished||!item.isCompleted) && (<div key={item.id} className="todo flex w-1/4 justify-between my-3">
+            return (showFinished||!item.isCompleted) && (<div key={item.id} className="todo flex md:w-1/2 justify-between my-3">
               <div className='flex gap-5'>
                 <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} />
                 <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
